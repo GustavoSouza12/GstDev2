8228496d28eea3266727b32bf622b5b04d8154e9
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-08-30 15:42:29 -0300

    first commit

---
+++ src/about/three-about.js
@@ -0,0 +1,119 @@
+import * as THREE from '../three.module';
+
+
+let container
+let camera
+let controls
+let renderer
+let scene
+let mesh
+
+function init() {
+
+container = document.querySelector( '#scene-container' )
+scene = new THREE.Scene()
+
+
+
+createCamera()
+createControls()
+createLights()
+createMeshes()
+createRenderer()
+
+renderer.setAnimationLoop( () => {
+
+    update()
+    render()
+
+} )
+
+}
+
+function createCamera(){
+    const fov = 35
+    const aspect = container.clientWidth / container.clientHeight
+    const near = 0.1
+    const far = 100
+    
+    camera = new THREE.PerspectiveCamera( fov, aspect, near, far )
+    camera.position.set( 10, 10, 10 )
+
+}
+function createControls(){
+
+    controls = new THREE.OrbitControls(camera, container)
+
+}
+
+function createLights(){
+
+    const ambientLight = new THREE.HemisphereLight(
+    0xddeeff, // bright sky color
+    0x202020, // dim ground color
+    5, // intensity
+    );
+
+    const mainLight = new THREE.DirectionalLight( 0xffffff, 5 )
+    mainLight.position.set( 10, 10, 10 )
+
+    scene.add( ambientLight )
+
+}
+
+function createMeshes(){
+
+    const geometry = new THREE.BoxBufferGeometry( 3, 3, 3 )
+    
+    const textureLoader = new THREE.TextureLoader()
+    const texture = textureLoader.load( 'https://avatars0.githubusercontent.com/u/62048657?s=460&u=05c2ae1c62489a3155f8395133cafc5b1ad18259&v=4' )
+
+    texture.encoding = THREE.sRGBEncoding
+    texture.anisotropy = 16
+    
+    const material = new THREE.MeshStandardMaterial( {
+    map: texture,
+    } )
+    
+    mesh = new THREE.Mesh( geometry, material )
+    scene.add( mesh )
+
+}
+
+function createRenderer(){
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } )
+    renderer.setSize( container.clientWidth, container.clientHeight )
+
+    renderer.setPixelRatio( window.devicePixelRatio )
+
+    renderer.gammaFactor = 2.2
+    renderer.gammaOutput = true
+
+    renderer.physicallyCorrectLights = true
+    container.appendChild( renderer.domElement )
+
+}
+
+
+
+function render() {
+
+    renderer.render( scene, camera )
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = container.clientWidth / container.clientHeight
+
+    camera.updateProjectionMatrix()
+    
+    renderer.setSize( container.clientWidth, container.clientHeight )
+
+}
+
+window.addEventListener( 'resize', onWindowResize )
+
+init()
+
---
+++ src/three-home.js
@@ -0,0 +1,201 @@
+import * as THREE from '../src/three.module.js';
+
+		
+
+		var container
+		var camera, scene, renderer, mesh, ambientLight
+
+		var instances = 5000;
+		var lastTime = 0;
+
+		var moveQ = new THREE.Quaternion( 0.5, 0.5, 0.5, 0.0 ).normalize();
+		var tmpQ = new THREE.Quaternion();
+		var tmpM = new THREE.Matrix4();
+		var currentM = new THREE.Matrix4();
+
+		init();
+		animate();
+
+		function init() {
+
+			container = document.getElementById( 'container' );
+
+			camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+
+		
+
+
+			scene = new THREE.Scene();
+			scene.background = new THREE.Color(  'black' );
+
+			// geometry
+
+			var geometry = new THREE.InstancedBufferGeometry();
+
+			// per mesh data x,y,z,w,u,v,s,t for 4-element alignment
+			// only use x,y,z and u,v; but x, y, z, nx, ny, nz, u, v would be a good layout
+			var vertexBuffer = new THREE.InterleavedBuffer( new Float32Array( [
+				// Front
+				- 1, 1, 1, 0, 0, 0, 0, 0,
+				1, 1, 1, 0, 1, 0, 0, 0,
+				- 1, - 1, 1, 0, 0, 1, 0, 0,
+				1, - 1, 1, 0, 1, 1, 0, 0,
+				// Back
+				1, 1, - 1, 0, 1, 0, 0, 0,
+				- 1, 1, - 1, 0, 0, 0, 0, 0,
+				1, - 1, - 1, 0, 1, 1, 0, 0,
+				- 1, - 1, - 1, 0, 0, 1, 0, 0,
+				// Left
+				- 1, 1, - 1, 0, 1, 1, 0, 0,
+				- 1, 1, 1, 0, 1, 0, 0, 0,
+				- 1, - 1, - 1, 0, 0, 1, 0, 0,
+				- 1, - 1, 1, 0, 0, 0, 0, 0,
+				// Right
+				1, 1, 1, 0, 1, 0, 0, 0,
+				1, 1, - 1, 0, 1, 1, 0, 0,
+				1, - 1, 1, 0, 0, 0, 0, 0,
+				1, - 1, - 1, 0, 0, 1, 0, 0,
+				// Top
+				- 1, 1, 1, 0, 0, 0, 0, 0,
+				1, 1, 1, 0, 1, 0, 0, 0,
+				- 1, 1, - 1, 0, 0, 1, 0, 0,
+				1, 1, - 1, 0, 1, 1, 0, 0,
+				// Bottom
+				1, - 1, 1, 0, 1, 0, 0, 0,
+				- 1, - 1, 1, 0, 0, 0, 0, 0,
+				1, - 1, - 1, 0, 1, 1, 0, 0,
+				- 1, - 1, - 1, 0, 0, 1, 0, 0
+			] ), 8 );
+
+			// Use vertexBuffer, starting at offset 0, 3 items in position attribute
+			var positions = new THREE.InterleavedBufferAttribute( vertexBuffer, 3, 0 );
+			geometry.setAttribute( 'position', positions );
+			// Use vertexBuffer, starting at offset 4, 2 items in uv attribute
+			var uvs = new THREE.InterleavedBufferAttribute( vertexBuffer, 2, 4 );
+			geometry.setAttribute( 'uv', uvs );
+
+			var indices = new Uint16Array( [
+				0, 2, 1,
+				2, 3, 1,
+				4, 6, 5,
+				6, 7, 5,
+				8, 10, 9,
+				10, 11, 9,
+				12, 14, 13,
+				14, 15, 13,
+				16, 17, 18,
+				18, 17, 19,
+				20, 21, 22,
+				22, 21, 23
+			] );
+
+			geometry.setIndex( new THREE.BufferAttribute( indices, 1 ) );
+
+			// material
+
+			var material = new THREE.MeshBasicMaterial();
+			material.map = new THREE.TextureLoader().load( 'https://avatars0.githubusercontent.com/u/62048657?s=460&u=05c2ae1c62489a3155f8395133cafc5b1ad18259&v=4' );
+
+			// per instance data
+
+			var matrix = new THREE.Matrix4();
+			var offset = new THREE.Vector3();
+			var orientation = new THREE.Quaternion();
+			var scale = new THREE.Vector3( 1, 1, 1 );
+			var x, y, z, w;
+
+			mesh = new THREE.InstancedMesh( geometry, material, instances );
+
+			for ( var i = 0; i < instances; i ++ ) {
+
+				// offsets
+
+				x = Math.random() * 100 - 50;
+				y = Math.random() * 100 - 50;
+				z = Math.random() * 100 - 50;
+
+				offset.set( x, y, z ).normalize();
+				offset.multiplyScalar( 5 ); // move out at least 5 units from center in current direction
+				offset.set( x + offset.x, y + offset.y, z + offset.z );
+
+				// orientations
+
+				x = Math.random() * 2 - 1;
+				y = Math.random() * 2 - 1;
+				z = Math.random() * 2 - 1;
+				w = Math.random() * 2 - 1;
+
+				orientation.set( x, y, z, w ).normalize();
+
+				matrix.compose( offset, orientation, scale );
+
+				mesh.setMatrixAt( i, matrix );
+
+			}
+
+			scene.add( mesh );
+
+			renderer = new THREE.WebGLRenderer();
+			renderer.setPixelRatio( window.devicePixelRatio );
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			container.appendChild( renderer.domElement );
+
+			if ( renderer.extensions.get( 'ANGLE_instanced_arrays' ) === null ) {
+
+				document.getElementById( 'notSupported' ).style.display = '';
+				return;
+
+			}
+
+			
+
+			window.addEventListener( 'resize', onWindowResize, false );
+
+		}
+
+		function onWindowResize() {
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize( window.innerWidth, window.innerHeight );
+
+		}
+
+		//
+
+		function animate() {
+
+			requestAnimationFrame( animate );
+
+			render();
+			
+
+		}
+
+		function render() {
+
+			var time = performance.now();
+
+			mesh.rotation.y = time * 0.00005;
+
+			var delta = ( time - lastTime ) / 5000;
+			tmpQ.set( moveQ.x * delta, moveQ.y * delta, moveQ.z * delta, 1 ).normalize();
+			tmpM.makeRotationFromQuaternion( tmpQ );
+
+			for ( var i = 0, il = instances; i < il; i ++ ) {
+
+				mesh.getMatrixAt( i, currentM );
+				currentM.multiply( tmpM );
+				mesh.setMatrixAt( i, currentM );
+
+			}
+
+			mesh.instanceMatrix.needsUpdate = true;
+
+			lastTime = time;
+
+			renderer.render( scene, camera );
+
+		}
+
